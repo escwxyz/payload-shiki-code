35d8931e2bc3916fedc28921f6f5ca33de6ae245
Jie <linsijia628@gmail.com>
2025-09-17 15:45:57 +0800

    chore: update hooks

--- src/hooks/update-block-name.ts
+++ src/hooks/update-block-name.ts
@@ -1,12 +1,10 @@
-import type { FieldHook } from "payload";
+import type { FieldHook } from 'payload';
 
 export const updateBlockName: FieldHook = async ({ value, blockData }) => {
-  if (typeof value === "string" && value.trim().length > 0) {
-    const fileName = value.trim();
+  if (blockData && typeof value === 'string') {
+    const title = value.trim();
 
-    if (blockData) {
-      blockData.blockName = fileName;
-    }
+    blockData.blockName = title.length > 0 ? title : undefined;
   }
 
   return value;
